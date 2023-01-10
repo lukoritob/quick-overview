97ccfbc05d8310e1150f72c5aab9772f78012abb
Brian Lascelles <lukoritob@gmail.com>
2023-01-10 11:08:38 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App ">
+      <header className='App-header'>
+        <div>
+          <img src="https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg" className="App-logo" alt="logo" />
+        </div>
+        <div className='my-link'>
+          <a
+            className="App-link"
+            href="https://www.propelleraero.com/dirtmate/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            propelleraero
+          </a>
+        </div>
       </header>
     </div>
   );
